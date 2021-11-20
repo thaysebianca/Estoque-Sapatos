525a2ff9bdefd2ce49d2f6374d1e179814efa724
RafaelDiney <91790222+RafaelDiney@users.noreply.github.com>
2021-11-20 11:23:12 -0300

    Create TESTE.c

---
+++ TESTE.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+ 
+
+    printf("%d\n", (123456%10));
+    printf("%d\n", (123456/10));
+    printf("%d\n", (123456%1000));
+    printf("%d\n", (123456%10000));
+    printf("%d\n", (123456%100000));
+    return 0;
+}
